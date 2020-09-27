c9a8efb6dcb93e92458af876b77cc4c5a1b25bbc
Nikitha <50269042+nikky1731@users.noreply.github.com>
2020-09-28 00:35:46 +0530

    Update Roll-the-dice.py

--- Roll-the-dice.py
+++ Roll-the-dice.py
@@ -8,6 +8,5 @@ while roll == "yes" or roll == "y":
     print ("Rolling the dices...")
     print ("The values are....")
     print (random.randint(min, max))
-    print (random.randint(min, max))
 
     roll_again = input("Do you want to roll the dices again?")

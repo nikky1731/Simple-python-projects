0272b1047372a3b74f66d64c61c6a02a17721756
nikky1731 <peddinikitha1731@gmail.com>
2025-08-18 21:07:04 +0100

    python basics second commit

--- Basics.py
+++ Basics.py
@@ -5,3 +5,12 @@ print("Hello world")
 name = input("Enter your name: ")
 print("Hello,", name, "! Welcome!")
 
+#printing variables
+s = "raj"
+print(s)
+
+#printing multiple variables
+s = "raj"
+y = "lakshmi"
+place = "hyderabad"
+print(s, y, place)

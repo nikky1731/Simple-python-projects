4d717564c5bf46b39ea513dcacef11784ee3eee2
nikky1731 <peddinikitha1731@gmail.com>
2025-08-20 16:06:18 +0100

    Basics file commit test

--- Basics.py
+++ Basics.py
@@ -1,3 +1,5 @@
+import sys
+
 #printing a string
 print("Hello world") 
 
@@ -14,3 +16,34 @@ s = "raj"
 y = "lakshmi"
 place = "hyderabad"
 print(s, y, place)
+
+#to checknsystem version
+print(sys.version)
+
+#making variable global
+x = "awesome"
+def myfunc():
+    global x
+    x = "fantastic"
+myfunc()
+print(x)
+
+#format string
+z = 9
+print(f'hi {z:.2f}')
+
+
+[print(x) for x in ['apple', 'banana', 'cherry']]
+
+#looping through dictionary 
+xx = {'type' : 'fruit', 'name' : 'apple'}
+for yy in xx.keys():
+  print(yy)
+
+for yy in xx.items():
+  print(yy)
+
+
+#to copy a tuple 
+yx = xx.copy()
+print(yx)

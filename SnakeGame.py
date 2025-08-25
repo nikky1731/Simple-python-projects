4869b9fe17a7e4a726b6d34fb1f7d1cffe19a24f
nikky1731 <peddinikitha1731@gmail.com>
2025-08-25 22:25:44 +0100

    commit of Snake Game changes

--- SnakeGame.py
+++ SnakeGame.py
@@ -4,14 +4,19 @@ import pygame
 
 #Pygame is a set of cross module Python Codes that is designed to program video games using Python language
 
-pygame.init()
+pygame.init() #Initializes all of the imported Pygame modules
 dis=pygame.display.set_mode((400,300))
-pygame.display.update()
 pygame.display.set_caption('Snake game by Nikky')
+ 
+blue=(0,0,255)
+red=(255,0,0)
+ 
 game_over=False
 while not game_over:
     for event in pygame.event.get():
-        print(event)   #prints out all the actions that take place on the screen
- 
+        if event.type==pygame.QUIT:
+            game_over=True
+    pygame.draw.rect(dis,blue,[200,150,10,10]) #draw.rect() can be used to draw rectangles in Pygame
+    pygame.display.update()
 pygame.quit()
 quit()
